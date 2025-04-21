0968b63dcb7ffb94a5e0f93e77ff5259e8cb5dfe
Vinay0357 <79446701+Vinay0357@users.noreply.github.com>
2025-04-21 14:59:54 +0530

    Added Small Logic
    Added Logic for test

--- app.py
+++ app.py
@@ -10,11 +10,12 @@
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', port=5000)
 
-print("Hello I'm Executing properlly")
+print("Hello I'm before my_func")
 def my_func():
   print("My Fun starts here")
   a = 10
   b = 20
   c = a + b
   print(f"The adding of {a} and {b} is: {c}")
-  print("My Fun ends here V2")
+  print("My Fun ends here")
+print("Hello I'm after my_func")
