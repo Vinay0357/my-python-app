d216f01488656519b162d4726df31a23028f17cd
Vinay0357 <79446701+Vinay0357@users.noreply.github.com>
2025-04-21 18:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# from flask import Flask, render_template
-# import os
+from flask import Flask, render_template
+import os
 
-# app  = Flask(__name__)
+app  = Flask(__name__)
 
-# @app.route("/")
-# def login():
-#     return render_template('login.html')
+@app.route("/")
+def login():
+    return render_template('login.html')
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
 
-print("Hello I'm before my_func")
-def my_func():
-  print("My Fun starts here")
-  a = 10
-  b = 20
-  c = a + b
-  print(f"The adding of {a} and {b} is: {c}")
-  print("My Fun ends here")
-print("Hello I'm after my_func")
+# print("Hello I'm before my_func")
+# def my_func():
+#   print("My Fun starts here")
+#   a = 10
+#   b = 20
+#   c = a + b
+#   print(f"The adding of {a} and {b} is: {c}")
+#   print("My Fun ends here")
+# print("Hello I'm after my_func")
