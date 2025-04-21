609611f4fe87c0061d0ba17bde5f78982ee57b3e
Vinay0357 <79446701+Vinay0357@users.noreply.github.com>
2025-04-21 13:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
-import os
+# from flask import Flask, render_template
+# import os
 
-app  = Flask(__name__)
+# app  = Flask(__name__)
 
-@app.route("/")
-def login():
-    return render_template('login.html')
+# @app.route("/")
+# def login():
+#     return render_template('login.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
+
+print("Hello I'm Executing properlly")
